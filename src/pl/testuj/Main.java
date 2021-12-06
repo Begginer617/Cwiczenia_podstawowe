4542c7df530ebea69259110c3c4d8b7cd8e5b0e4
Wojtek Potocki <szkolenia@wojtekpotocki.pl>
2021-12-06 18:36:24 +0100

    polimorfizm

--- src/pl/testuj/Main.java
+++ src/pl/testuj/Main.java
@@ -1,11 +1,49 @@
 package pl.testuj;
 
 import pl.testuj.oop.Animal;
+import pl.testuj.oop.Cat;
+import pl.testuj.oop.Dog;
+import pl.testuj.oop.Elephant;
 
 public class Main {
 
     public static void main(String[] args) {
         // klasa Animal jest abstrakcyjna, więc nie mogę stworzyć jej instancji
 //	    Animal animal = new Animal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        Animal animalDog = new Dog();
+        Animal animalCat = new Cat();
+
+        animalDog.useVoice();
+        animalCat.useVoice();
+
+        Dog[] dogs = new Dog[2];
+        Cat[] cats = new Cat[2];
+
+        Animal[] animals = new Animal[5];
+        animals[0] = dog;
+        animals[1] = animalDog;
+        animals[2] = cat;
+        animals[3] = animalCat;
+        animals[4] = new Elephant();
+
+        for (int i = 0; i < animals.length; i++) {
+            voice(animals[i]);
+        }
     }
+
+    public static void voice(Animal animal) {
+        animal.useVoice();
+    }
+
+//    public static void voiceDog(Dog dog) {
+//        dog.useVoice();
+//    }
+//
+//    public static void voiceCat(Cat cat) {
+//        cat.useVoice();
+//    }
 }
---
+++ src/pl/testuj/oop/Elephant.java
@@ -0,0 +1,13 @@
+package pl.testuj.oop;
+
+public class Elephant extends Animal {
+    @Override
+    public void useVoice() {
+        System.out.println("Trututu");
+    }
+
+    @Override
+    public int getAge(int x) {
+        return 0;
+    }
+}

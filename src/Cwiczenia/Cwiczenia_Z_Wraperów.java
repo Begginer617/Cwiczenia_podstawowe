78ee11cd761d5909b536934c689ee1cfa767b6ba
Begginer617 <75578722+Begginer617@users.noreply.github.com>
2022-01-23 19:03:04 +0100

    Initial commit wrapery cwiczenia

---
+++ src/Cwiczenia/Cwiczenia_Z_Wraperów.java
@@ -0,0 +1,12 @@
+package Cwiczenia;
+
+public class Cwiczenia_Z_Wraperów extends Klasa_Bazowa {
+
+    Double mojDouble = 8.8900;
+
+
+
+
+
+}
+
---
+++ src/Cwiczenia/Klasa_Bazowa.java
@@ -0,0 +1,16 @@
+package Cwiczenia;
+
+public class Klasa_Bazowa {
+
+    private static Object mojDouble;
+
+    public static void main(String[] args) {
+
+        mojDouble;
+
+
+
+
+    }
+
+}
